ceecfedab7005abc64f4eb05604dbd6c6d7e3beb
Samo123782 <amarasinghesamod@gmail.com>
2025-05-02 11:32:02 +0530

    added new path

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 st.set_page_config(page_title="Sri Lanka Displacements Dashboard", page_icon="🇱🇰", layout="wide")
 
 #  Load Dataset 
-df = pd.read_csv("/Users/samodamarasinghe/Desktop/dspl_cw2/internal-displacements-new-displacements-associated-with-disasters_lka.csv")
+df = pd.read_csv("internal-displacements-new-displacements-associated-with-disasters_lka.csv")
 
 #  Data Cleaning 
 df.columns = df.columns.str.strip()
